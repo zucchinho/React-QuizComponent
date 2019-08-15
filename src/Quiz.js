bdf68b7ac752e357948fb7b1d51bbe176725ffa7
ucas_lewc <l.clegg@ucas.ac.uk>
2019-08-15 15:42:21 +0100

    module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 }
   }
 
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 == quizData.quiz_questions.length
@@ -24,6 +30,7 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
+    }
+  }
+
   render() {
     return (
       <main>
@@ -10,9 +16,15 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => (
+                <QuizQuestionButton
+                  key={index}
+                  button_text={answer_option[0]}
+                  clickHandler={this.handleClick.bind(this)}
+                />
+              ),
+            )}
           </ul>
         </section>
       </main>
