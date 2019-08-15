650a9a13dd6d23a4f40dbc8cf333c31abfe169ca
ucas_lewc <l.clegg@ucas.ac.uk>
2019-08-15 15:54:03 +0100

    complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,6 +17,12 @@ class Quiz extends Component {
     })
   }
 
+  handleResetClick() {
+    this.setState(state => ({
+      quiz_position: 1,
+    }))
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 == quizData.quiz_questions.length
@@ -24,7 +30,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
